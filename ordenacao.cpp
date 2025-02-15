36cb5c3e3019a9159fb531e550eb9b8b60ed7586
Robson Alencastro <binhoalves.14@hotmail.com>
2025-02-15 17:28:07 -0300

    Implementação merge_sort

--- ordenacao.cpp
+++ ordenacao.cpp
@@ -50,4 +50,42 @@ void insercao(int a[], unsigned int t)
 }
 
 /* TODO: Implementar função */
-void merge_sort(int a[], unsigned int t);
+void merge(int a[], int i1, int j1, int i2, int j2){
+    int* temp = new int[j2 - i1 + 1]; 
+    int i = i1, j = i2, k = 0;
+
+
+    while (i <= j1 && j <= j2) {
+        if (a[i] < a[j]) {
+            temp[k++] = a[i++];
+        } else {
+            temp[k++] = a[j++];
+        }
+    }
+
+    while (i <= j1) {
+        temp[k++] = a[i++];
+    }
+
+  
+    while (j <= j2) {
+        temp[k++] = a[j++];
+    }
+
+  
+    for (i = i1, k = 0; i <= j2; i++, k++) {
+        a[i] = temp[k];
+    }
+
+    delete[] temp;
+}
+
+void merge_sort(int a[], int primeiro, int ultimo){
+    if (primeiro < ultimo) {
+        int meio = (primeiro + ultimo) / 2;
+        merge_sort(a, primeiro, meio);
+        merge_sort(a, meio + 1, ultimo);
+        merge(a, primeiro, meio, meio + 1, ultimo);     }
+}
+
+
