98fa783eb8c0121e834575eb0c2d68430416d447
Robson Alencastro <binhoalves.14@hotmail.com>
2025-02-15 16:45:30 -0300

    Implementação do algoritmo de ordenação com insertion sort.

--- ordenacao.cpp
+++ ordenacao.cpp
@@ -14,10 +14,10 @@ bool ordenado(int a[], unsigned int t)
 }
 
 /* TODO: Implementar função */
-void selecao(int a[], unsigned int t)
+void selecao(int num[], unsigned int tam)
 {
     int min, aux;
-    for(i = 0; i < (tam-1); i++){
+    for(int i = 0; i < (tam-1); i++){
         min = i;
         for(int j = (i+1); j < tam; j++){
             if(num[j] < num[min]){
@@ -35,6 +35,18 @@ void selecao(int a[], unsigned int t)
 /* TODO: Implementar função */
 void insercao(int a[], unsigned int t)
 {
+    int x, j;
+    for (unsigned int i = 1; i < t; i++) {  
+        x = a[i];
+        j = i - 1;
+
+        while (j >= 0 and a[j] > x) {
+            a[j + 1] = a[j];
+            j--;
+        }
+
+        a[j + 1] = x;
+    }
 }
 
 /* TODO: Implementar função */
